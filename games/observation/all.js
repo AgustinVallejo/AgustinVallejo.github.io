9bc9fd2892baf39c69ed59ae6f66ad8ef8bef7c7
Agustín Vallejo <agusin.vallejo@udea.edu.co>
2022-02-20 20:24:28 -0500

    Prevent scrolling

--- games/observation/all.js
+++ games/observation/all.js
@@ -29,3 +29,9 @@ function draw() {
     t0++;
     clicked = false;
 }
+
+window.addEventListener("keydown", function(e) {
+    if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+        e.preventDefault();
+    }
+}, false);
