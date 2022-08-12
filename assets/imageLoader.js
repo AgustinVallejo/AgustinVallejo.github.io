4f6bcc16cfdbb6c063c92eadfcff5545fee97373
AgustinVallejo <agustin.vallejo@udea.edu.co>
2022-08-12 13:40:23 -0500

    Finally making the art gallery cleaner!

--- assets/imageLoader.js
+++ assets/imageLoader.js
@@ -68,10 +68,12 @@ let gallery = document.getElementById("gallery")
 
 files.forEach(name => {
     let strHTML = `` +
-    `<a href="/assets/art/${name}" data-toggle="lightbox"`+
-    `data-gallery="example-gallery" class="col-sm-4 img-thumbnail mx-auto centered">` +
-    `<img src="/assets/art/${name}" class="img-fluid">` +
-    `</a>`
+    `<a href="/assets/art/${name}" data-toggle="lightbox" 
+    data-gallery="example-gallery"
+    class="col-sm-3 img-thumbnail d-flex flex-wrap align-items-center bg-dark" style="margin:5;"
+    data-max-height="600">
+    <img src="/assets/art/${name}" class="img-fluid" style="border-radius:5px;">
+    </a>`
     gallery.innerHTML += strHTML
 })
 
