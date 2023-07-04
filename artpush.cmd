65c3d161323fe1fa8e26b8a95abbdaeaf8cb20e9
AgustinVallejo <agustin.vallejo@udea.edu.co>
2023-07-04 12:09:03 -0500

    Adding automatic art commits

---
+++ artpush.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+REM Run the Python script
+python assets/fileUpdater.py
+
+REM Prompt for confirmation to commit and push
+set /p "commit_confirmation=Do you want to commit and push the changes? (y/n): "
+
+IF /I "%commit_confirmation%"=="y" (
+    REM Make the commit
+    git commit -am "Automatic commit of artwork"
+
+    REM Push to the main branch
+    git push origin main
+
+    echo Changes committed and pushed successfully.
+) ELSE (
+    echo No changes were committed or pushed.
+)
