e0027e33bdb9b73fbcb0ad7126ce22bb19bcc6d2
AgustinVallejo <agustin.vallejo@udea.edu.co>
2023-11-01 12:24:01 -0500

    Maybe updating auto commit

--- artpush.cmd
+++ artpush.cmd
@@ -6,13 +6,20 @@ python assets/fileUpdater.py
 git status
 
 REM Prompt for confirmation to commit and push
-set /p "commit_confirmation=Do you want to "git add ." and commit and push the changes? (y/n): "
+set /p "commit_confirmation=Do you want to 'git add .' and commit and push the changes? (y/n): "
 
 IF /I "%commit_confirmation%"=="y" (
+    REM Prompt for a custom commit message or use a default message if left blank
+    set /p "custom_message=Enter a custom commit message (or press Enter to use the default): "
+
+    if "%custom_message%"=="" (
+        set "custom_message=Automatic commit of artwork"
+    )
+    
     git add .
 
-    REM Make the commit
-    git commit -am "Automatic commit of artwork"
+    REM Make the commit with the custom message
+    git commit -am "%custom_message%"
 
     REM Push to the main branch
     git push origin main
